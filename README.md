71d181fbe0346a1e22532f85d61c213c9347c3f7
anton_galich <inthesky133@gmail.com>
2023-02-01 09:55:31 +0300

    настройка проекта - добавление переменных и темизации

---
+++ README.md
@@ -0,0 +1,1 @@
+# Feature slice design architecture проект на React
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React, {Suspense} from 'react';
-import './index.scss';
+import React, {Suspense, useContext, useEffect} from 'react';
+import './styles/index.scss';
 import {Link, Route, Routes} from "react-router-dom";
-import {AboutPageAsync} from "./components/pages/AboutPage/AboutPage.async";
-import {MainPageAsync} from "./components/pages/MainPage/MainPage.async";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {useTheme} from "./theme/useTheme";
 
 const App = () => {
+  const {theme, toggleTheme} = useTheme();
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>Toggle theme</button>
       <Link to={'/'}>Main</Link>
       <Link to={'/about'}>About</Link>
       <Routes>
