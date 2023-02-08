72bf792d0868bbe2bbe69a08290bc5e72b752124
anton_galich <inthesky133@gmail.com>
2023-02-08 08:30:24 +0300

    компоненты - добавлены страница "не найдено", лоадер

--- src/shared/config/routerConfig/routeConfig.tsx
+++ src/shared/config/routerConfig/routeConfig.tsx
@@ -2,26 +2,34 @@ import { type RouteProps } from 'react-router-dom'
 import { MainPage } from 'pages/MainPage'
 import { AboutPage } from 'pages/AboutPage'
 import { Suspense } from 'react'
+import { NotFoundPage } from 'pages/NotFoundPage'
+import { PageLoader } from 'widgets/PageLoader'
 
 export const AppRoutes = {
   MAIN: '/',
-  ABOUT: 'about'
+  ABOUT: 'about',
+  NOT_FOUND: '*',
 } as const
 
 type AppRoutesType = keyof typeof AppRoutes
 
 export const RoutePath: Record<AppRoutesType, string> = {
   MAIN: '/',
-  ABOUT: '/about'
+  ABOUT: '/about',
+  NOT_FOUND: '*',
 }
 
 export const RouteConfig: Record<AppRoutesType, RouteProps> = {
   MAIN: {
     path: RoutePath.MAIN,
-    element: <Suspense fallback={<div>Loading...</div>}><MainPage /></Suspense>
+    element: <Suspense fallback={<PageLoader/>}><MainPage /></Suspense>,
   },
   ABOUT: {
     path: RoutePath.ABOUT,
-    element: <Suspense fallback={<div>Loading...</div>}><AboutPage /></Suspense>
-  }
+    element: <Suspense fallback={<PageLoader/>}><AboutPage /></Suspense>,
+  },
+  NOT_FOUND: {
+    path: RoutePath.NOT_FOUND,
+    element: <NotFoundPage />,
+  },
 }
