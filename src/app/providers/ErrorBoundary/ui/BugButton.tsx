25ee3a250f016b408d8a5549987c5c7ed2aa8770
anton_galich <inthesky133@gmail.com>
2023-02-12 13:16:37 +0300

    настройка проекта - добавлен ErrorBoundary

---
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react'
+import { AppButton } from 'shared/ui/AppButton/AppButton'
+
+// Для тестирования ErrorBoundary
+export const BugButton = () => {
+  const [error, setError] = useState(false)
+
+  const onThrow = () => { setError(true) }
+
+  useEffect(() => {
+    if (error) {
+      throw new Error('Error')
+    }
+  }, [error])
+
+  return (
+    // eslint-disable-next-line i18next/no-literal-string
+    <AppButton onClick={onThrow}>Throw Err</AppButton>
+  )
+}
---
+++ src/widgets/ErrorComponent/ui/ErrorComponent.tsx
@@ -0,0 +1,27 @@
+import React, { type FC } from 'react'
+import { classNames } from 'shared/lib/classNames/classNames'
+import cls from './ErrorComponent.module.scss'
+import { useTranslation } from 'react-i18next'
+import { AppButton } from 'shared/ui/AppButton/AppButton'
+
+interface ErrorComponentProps {
+  className?: string
+}
+
+export const ErrorComponent: FC<ErrorComponentProps> = (props) => {
+  const { className } = props
+  const { t } = useTranslation()
+
+  const reloadPage = () => {
+    window.location.reload()
+  }
+
+  return (
+    <div className={classNames(cls.errorComponent, {}, [className])}>
+      <h1>{t('Произошла непредвиденная ошибка')}</h1>
+      <AppButton onClick={reloadPage}>
+        {t('Обновить страницу')}
+      </AppButton>
+    </div>
+  )
+}
