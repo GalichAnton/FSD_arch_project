9f6422236cc9023949d61eade7e5519a1957fdcb
anton_galich <inthesky133@gmail.com>
2023-03-08 12:17:46 +0300

    фикс

--- src/entity/Profile/model/selectors/getProfileValidateErrors/getProfileValidateErrors.test.ts
+++ src/entity/Profile/model/selectors/getProfileValidateErrors/getProfileValidateErrors.test.ts
@@ -1,24 +1,25 @@
-import { StateSchema } from 'app/providers/StoreProvider';
-import { ValidateProfileError } from 'entities/Profile';
-import { getProfileValidateErrors } from './getProfileValidateErrors';
+import { type DeepPartial } from '@reduxjs/toolkit'
+import { type StateSchema } from 'app/providers/StoreProvider'
+import { ValidateProfileError } from 'entity/Profile'
+import { getProfileValidateErrors } from './getProfileValidateErrors'
 
 describe('getProfileValidateErrors.test', () => {
-    test('should work with filled state', () => {
-        const state: DeepPartial<StateSchema> = {
-            profile: {
-                validateErrors: [
-                    ValidateProfileError.SERVER_ERROR,
-                    ValidateProfileError.INCORRECT_AGE,
-                ],
-            },
-        };
-        expect(getProfileValidateErrors(state as StateSchema)).toEqual([
-            ValidateProfileError.SERVER_ERROR,
-            ValidateProfileError.INCORRECT_AGE,
-        ]);
-    });
-    test('should work with empty state', () => {
-        const state: DeepPartial<StateSchema> = {};
-        expect(getProfileValidateErrors(state as StateSchema)).toEqual(undefined);
-    });
-});
+  test('should work with filled state', () => {
+    const state: DeepPartial<StateSchema> = {
+      profile: {
+        validateErrors: [
+          ValidateProfileError.SERVER_ERROR,
+          ValidateProfileError.INCORRECT_AGE,
+        ],
+      },
+    }
+    expect(getProfileValidateErrors(state as StateSchema)).toEqual([
+      ValidateProfileError.SERVER_ERROR,
+      ValidateProfileError.INCORRECT_AGE,
+    ])
+  })
+  test('should work with empty state', () => {
+    const state: DeepPartial<StateSchema> = {}
+    expect(getProfileValidateErrors(state as StateSchema)).toEqual(undefined)
+  })
+})
