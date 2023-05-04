1cad6b36dea8df336e878e6daa7ed915265ebfc7
anton_galich <inthesky133@gmail.com>
2023-05-04 16:44:06 +0300

    компонениы - дравер, хук useDevice

--- src/features/notificationButton/ui/NotificationButton/NotificationButton.tsx
+++ src/features/notificationButton/ui/NotificationButton/NotificationButton.tsx
@@ -1,11 +1,13 @@
 import { classNames } from 'shared/lib/classNames/classNames'
-import React, { memo } from 'react'
+import { memo, useCallback, useState } from 'react'
 import { AppButton, AppButtonVariant } from 'shared/ui/AppButton/AppButton'
 import { Icon } from 'shared/ui/Icon/Icon'
 import NotificationIcon from 'shared/assets/icons/notification-20-20.svg'
 import { NotificationList } from 'entity/Notification'
 import { Popover } from 'shared/ui/Popups'
 import cls from './NotificationButton.module.scss'
+import { useDevice } from 'shared/lib/hooks/useDevice/useDevice'
+import { Drawer } from 'shared/ui/Drawer/Drawer'
 
 interface NotificationButtonProps {
   className?: string
@@ -14,17 +16,45 @@ interface NotificationButtonProps {
 export const NotificationButton = memo((props: NotificationButtonProps) => {
   const { className } = props
 
-  return (
-        <Popover
-            className={classNames(cls.NotificationButton, {}, [className])}
-            direction="bottom left"
-            trigger={(
-                <AppButton variant={AppButtonVariant.CLEAR}>
-                    <Icon Svg={NotificationIcon} inverted />
-                </AppButton>
-            )}
-        >
-            <NotificationList className={cls.notifications} />
-        </Popover>
+  const [isOpen, setIsOpen] = useState(false)
+  const isMobile = useDevice()
+
+  const onOpenDrawer = useCallback(() => {
+    setIsOpen(true)
+  }, [])
+
+  const onCloseDrawer = useCallback(() => {
+    setIsOpen(false)
+  }, [])
+
+  const trigger = (
+    <AppButton onClick={onOpenDrawer} variant={AppButtonVariant.CLEAR}>
+        <Icon Svg={NotificationIcon} inverted />
+    </AppButton>
   )
+
+  if (isMobile) {
+    return (
+      <>
+        {trigger}
+        <Drawer isOpen={isOpen} onClose={onCloseDrawer}>
+            <NotificationList />
+        </Drawer>
+      </>
+    )
+  } else {
+    return (
+      <Popover
+        className={classNames(cls.NotificationButton, {}, [className])}
+        direction="bottom left"
+        trigger={(
+            <AppButton variant={AppButtonVariant.CLEAR}>
+                <Icon Svg={NotificationIcon} inverted />
+            </AppButton>
+        )}
+        >
+          <NotificationList className={cls.notifications} />
+      </Popover>
+    )
+  }
 })
--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -9,6 +9,7 @@ import React, {
 import { classNames } from 'shared/lib/classNames/classNames'
 import { Portal } from '../Portal/Portal'
 import cls from './Modal.module.scss'
+import { Overlay } from '../Overlay/Overlay'
 
 interface ModalProps {
   className?: string
@@ -87,10 +88,9 @@ export const Modal: FC<ModalProps> = (props) => {
   return (
     <Portal>
       <div className={classNames(cls.modal, mods, [className, theme, 'app_modal'])}>
-        <div className={cls.overlay} onClick={closeHandler}>
-          <div className={cls.content} onClick={onContentClick}>
-            {children}
-          </div>
+      <Overlay onClick={closeHandler} />
+        <div className={cls.content} onClick={onContentClick}>
+          {children}
         </div>
       </div>
     </Portal>
